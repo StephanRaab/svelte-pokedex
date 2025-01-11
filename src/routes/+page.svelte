a7b284d846a57c98fbd8081c0cb6dc66c38987e0
Stephan Raab <sarcartist@gmail.com>
2025-01-11 13:35:42 -0500

    use updated svelte fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,10 @@
 
 <script lang="ts">
- import {generations} from './generations'    
- export let data;
+ import type {PageData} from './$types'; 
+
+ export let data: PageData;
 </script>
 
-{#each data.monsters as monster}
-    <p>[] {monster.name}</p>
-{/each}
-
-{#each generations as gen (gen.id)}
-    <h2>{gen.name}</h2>
-    <p>Games:</p>
-    <ul>
-        {#each gen.games as game}
-           <li>{game}</li>
-        {/each}
-    </ul>
-    <p>Main Region: {gen.main_region}</p>
+{#each data.monsters as monster(monster.url)}
+    <h3>[{monster.id}] {monster.name}</h3>
 {/each}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,28 @@
-export const load = async () => {
+import type { PageLoad } from "./$types";
+
+type IndexMonster = {
+  name: string;
+  url: string;
+  id: number;
+};
+
+export const load = (async ({ fetch }) => {
   // this is for SSR
+
   const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const json = await res.json();
+  const monsters = json.results.map((monster: IndexMonster) => {
+    const splitUrl = monster.url.split("/");
+    const id = splitUrl[splitUrl.length - 2];
+
+    return {
+      name: monster.name,
+      url: monster.url,
+      id,
+    };
+  });
 
   return {
-    monsters: json.results,
+    monsters,
   };
-};
+}) satisfies PageLoad;
