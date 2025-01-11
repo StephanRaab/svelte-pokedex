242da8fb1b3f2f551e2ff8afe34bea825a7356de
Stephan Raab <sarcartist@gmail.com>
2025-01-11 14:03:32 -0500

    start adding pokemon images and some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,64 @@
  export let data: PageData;
 </script>
 
-{#each data.monsters as monster(monster.url)}
-    <h3>[{monster.id}] {monster.name}</h3>
-{/each}
+<div class="monsters">
+    {#each data.monsters as monster(monster.id)}
+        
+        <div class="monster">
+            <div class="monster-id">
+                {monster.id}
+            </div>
+            <div class="monster-content">
+                <img src="{monster.image}" alt="{monster.name} image">
+                 <h3>{monster.name}</h3>
+            </div>            
+        </div>
+    {/each}
+</div>
+
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: monospace;        
+    }
+    
+    .monsters {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+    .monster {
+        width: 120px;        
+        padding: .5em;
+        background-color: #eee;   
+        border-radius: .5rem;
+        cursor: pointer;
+        transition: .3s ease-in-out;
+        border: 1px solid #ddd;
+    }
+    .monster:hover {
+        background-color: #d1d1d1;
+        transition: .3s ease-in-out;
+    }
+    .monster-id {
+        background: white;        
+        border-radius: 100%;
+        height: 2rem;
+        width: 2rem;
+        text-align: center;
+        vertical-align: middle;
+        padding-top: .5rem;                
+        color: #1d1d1d;
+        font-weight: 800;
+        border: 1px solid #ddd;
+    }
+    .monster-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;        
+    }
+</style>
