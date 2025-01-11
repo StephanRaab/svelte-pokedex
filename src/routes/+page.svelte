ee1242a939cd977cce12ff4f9dd8a7bc42a939e7
Stephan Raab <sarcartist@gmail.com>
2025-01-11 13:02:03 -0500

    for each loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,16 @@
     greeting: 'hello',
     name: 'bob'
  };
+
+ const data = [
+    "red","blue", "yellow"
+]
 </script>
 
 <h1>{greetingObject.greeting} {greetingObject.name}</h1>
+
+{#each data as color}
+    <ul>
+        <li>{color}</li>
+    </ul>
+{/each}
